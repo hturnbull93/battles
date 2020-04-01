4abba00e2070cb4e61cc0ea2a81bad44abace817
Faye Carter <fayecarter333@gmail.com>
2020-04-01 16:20:51 +0100

    Attack action added

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'sinatra/base'
 class Battles < Sinatra::Base
 
 enable :sessions
-set :session_secret, "Player Store"
+set :session_secret, "New"
 
   get '/' do
     erb :index
@@ -17,10 +17,21 @@ set :session_secret, "Player Store"
   end
   
   get '/play' do
+
+    p session
     @p1name = session[:p1name]
     @p2name = session[:p2name]
     @p2hp = session[:p2hp]
+    p session
+    @result = session[:result]
+    p session
     erb :play
   end
+
+  post '/action' do
+    session[:result] = params[:Action]
+    redirect '/play'
+  end
+
 end
 
---
+++ spec/features/attack_spec.rb
@@ -0,0 +1,7 @@
+feature 'Attack players' do
+  scenario 'gives confirmation of attack' do
+    sign_in_and_play
+    click_button('Attack')
+    expect(page).to have_content 'Attack made!'
+  end
+end
