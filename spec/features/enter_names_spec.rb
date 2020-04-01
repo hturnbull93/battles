46dff982ed4e2f9a8fa6d737edd4b62e95417dfb
Faye Carter <fayecarter333@gmail.com>
2020-04-01 15:45:10 +0100

    added Test helpers

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Enter names' do
   scenario 'enter names and see them displayed' do
-    visit '/'
-    fill_in('p1name', with: 'John')
-    fill_in('p2name', with: 'Pete')
-    click_button('Go!')
+    sign_in_and_play
     expect(page).to have_content 'John vs Pete!'
   end
 end
--- spec/features/view_hp_spec.rb
+++ spec/features/view_hp_spec.rb
@@ -1,9 +1,6 @@
 feature 'View Hit Points' do
   scenario 'see the polayers hitpoints displayed' do
-    visit '/'
-    fill_in('p1name', with: 'John')
-    fill_in('p2name', with: 'Pete')
-    click_button('Go!')
+    sign_in_and_play
     expect(page).to have_content 'Player 2 HP: 100'
   end
 end
