76a898fd41ca636c45927f77ff64042cc21a469a
Student <student@Makerss-MacBook-Air-2.local>
2020-04-01 15:25:45 +0100

    rf to use session params and http redirect top /play

--- app.rb
+++ app.rb
@@ -2,13 +2,22 @@ require 'sinatra/base'
 
 class Battles < Sinatra::Base
 
+enable :sessions
+
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @p1name = params[:p1name]
-    @p2name = params[:p2name]
+    session[:p1name] = params[:p1name]
+    session[:p2name] = params[:p2name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @p1name = session[:p1name]
+    @p2name = session[:p2name]
     erb :play
   end
 end
+
