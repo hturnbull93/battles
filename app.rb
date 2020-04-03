821a5b8c9f334e3f200c2ba6ace4b38128464919
Student <student@Makerss-MacBook-Air-2.local>
2020-04-03 12:58:47 +0100

    now using class variables rather than global variables

--- app.rb
+++ app.rb
@@ -11,25 +11,27 @@ set :session_secret, "New"
   end
 
   post '/names' do
-    $game = Game.new(params[:p1name], params[:p2name])
+    @game = Game.create(params[:p1name], params[:p2name])
     redirect '/play'
   end
   
+  before do
+    @game = Game.instance
+  end
+
   get '/play' do
-    @game = $game
     @result = session[:result]
     erb :play
   end
 
   get '/game-over' do
-    @game = $game
     erb :game_over
   end
 
   post '/action' do
     session[:result] = params[:action]
-    $game.attack
-    if !$game.winner.nil?
+    @game.attack
+    if !@game.winner.nil?
       redirect '/game-over' 
     else
       redirect '/play' 
--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,14 @@
 require_relative 'player'
 
 class Game
+  def self.create(p1_name, p2_name)
+    @game = Game.new(p1_name, p2_name)
+  end
+
+  def self.instance
+    @game
+  end
+
   def initialize(p1_name, p2_name, player_class = Player)
     @p1 = player_class.new(p1_name)
     @p2 = player_class.new(p2_name)
