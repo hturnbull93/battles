92e42f8331133c6c8e6f4470b4511bf861d00270
Student <student@Makerss-MacBook-Air-2.local>
2020-04-03 10:56:36 +0100

    game accepts 2 players at construction

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,15 @@
+require_relative 'player'
+
 class Game
+  def initialize(p1_name, p2_name, player_class = Player)
+    @p1 = player_class.new(p1_name)
+    @p2 = player_class.new(p2_name)
+  end
+  
+  attr_reader :p1, :p2
+
   def attack(target)
     target.take_damage
   end
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -8,10 +8,29 @@ describe Game do
   let(:dave) { double(:player) }
   let(:jim) { double(:player) }
 
+  # Player class double
+  let(:player_class) do
+    player_class = double(:player_class)
+    allow(player_class).to receive(:new).with('Dave').and_return(dave)
+    allow(player_class).to receive(:new).with('Jim').and_return(jim)
+    player_class
+  end
+
+  subject { described_class.new('Dave', 'Jim', player_class) }
+
+  describe 'Players stored' do
+    it '#p1 returns the player instance' do
+      expect(subject.p1).to eq dave
+    end
+    it '#p2 returns the player instance' do
+      expect(subject.p2).to eq jim
+    end
+  end
+
   describe '#attack' do
     it 'attack a player' do
       expect(jim).to receive :take_damage
-      subject.attack(jim)
+      subject.attack(subject.p2)
     end
   end
 end
