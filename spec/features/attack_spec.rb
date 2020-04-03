fe510cdc7b7f2ec701c9ba9f156288d0b77d44ef
Student <student@Makerss-MacBook-Air-2.local>
2020-04-03 12:21:43 +0100

    attacking with p2

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -8,6 +8,13 @@ feature 'Attack players' do
   scenario 'p1 attacks p2 and reduces p2 hp' do
     sign_in_and_play
     click_button('Attack')
-    expect(page).to have_content '90'
+    expect(page).to have_content 'Jim HP: 90'
+  end
+
+  scenario 'p2 attacks p1 and reduces p1 hp' do
+    sign_in_and_play
+    click_button('Attack')
+    click_button('Attack')
+    expect(page).to have_content 'Jim HP: 90'
   end
 end
