4fd0c1cbf81261ec64765419af56364847e4a4a1
Student <student@Makerss-MacBook-Air-2.local>
2020-04-03 12:22:38 +0100

    game has winner if a player hp <= 0

--- lib/game.rb
+++ lib/game.rb
@@ -6,16 +6,24 @@ class Game
     @p2 = player_class.new(p2_name)
     @active = @p1
     @target = @p2
+    @winner = nil
   end
   
-  attr_reader :p1, :p2, :active, :target
+  attr_reader :p1, :p2, :active, :target, :winner
 
   def attack
     @target.take_damage
     switch_roles
+    check_winner
   end
 
   private
+
+  def check_winner
+    @winner = @p1 if @p2.hp <= 0
+    @winner = @p2 if @p1.hp <= 0
+  end
+
 
   def switch_roles
     @active, @target = @target, @active
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,18 +3,23 @@ require 'game'
 describe Game do
   
   # Player instance double
-  let(:dave) { double(:dave, take_damage: 'Ow') }
-  let(:jim) { double(:jim, take_damage: 'Ow') }
+  let(:dave) { double(:dave, take_damage: 'Ow', hp: 100) }
+  let(:jim) { double(:jim, take_damage: 'Ow', hp: 100) }
+  let(:weakened) { double(:weakened, take_damage: 'Ow', hp: 0) }
 
   # Player class double
   let(:player_class) do
     player_class = double(:player_class)
     allow(player_class).to receive(:new).with('Dave').and_return(dave)
     allow(player_class).to receive(:new).with('Jim').and_return(jim)
+    allow(player_class).to receive(:new).with('Weakened').and_return(weakened)
     player_class
   end
 
+  # Isolated game scenarios
   subject { described_class.new('Dave', 'Jim', player_class) }
+  let(:one_sided_1) { described_class.new('Dave', 'Weakened', player_class) }
+  let(:one_sided_2) { described_class.new('Weakened', 'Jim', player_class) }
 
   describe 'Players stored' do
     it '#p1 returns the player instance' do
@@ -40,5 +45,17 @@ describe Game do
       expect(jim).to receive :take_damage
       subject.attack
     end
+  end
+
+  describe '#winner' do
+    it 'if a player 2 hp <= 0, winner is player 1' do
+      one_sided_1.attack
+      expect(one_sided_1.winner).to eq dave
+    end
+    it 'if a player 1 hp <= 0, winner is player 2' do
+      one_sided_2.attack
+      expect(one_sided_2.winner).to eq jim
+    end
+
   end
 end
