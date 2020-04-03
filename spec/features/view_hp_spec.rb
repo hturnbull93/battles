884417697a18c2940f3a326e785cee341e362fe0
Student <student@Makerss-MacBook-Air-2.local>
2020-04-03 10:57:46 +0100

    rf to display name rather than player 2

--- spec/features/view_hp_spec.rb
+++ spec/features/view_hp_spec.rb
@@ -1,6 +1,6 @@
 feature 'View Hit Points' do
   scenario 'see the polayers hitpoints displayed' do
     sign_in_and_play
-    expect(page).to have_content 'Player 2 HP: 100'
+    expect(page).to have_content 'Jim HP: 100'
   end
 end
